41ed03f4c1c2ad2ad2ab46d8c83d064b359038ff
snowboiii <136127828+snowboiii@users.noreply.github.com>
2025-06-23 14:26:16 +0200

    results.js different questions

--- results/results.js
+++ results/results.js
@@ -1,30 +1,30 @@
 const titles = [
-    ["You see someone drop a wallet with 200€ in it. No one else notices.", 0],
-    ["Your friend's horrible artwork is being shown at school. They ask you if you like it, looking proud and nervous.", 1],
-    ["You're about to fail a big exam. A friend secretly gives you the answers.", 2],
-    ["You forgot your best friend's birthday. You can lie and say the gift is late, or tell the truth.", 3],
-    ["A trolley is heading towards 5 people. You can pull the lever to divert it to the other track, killing 1 person instead.", 4],
-    ["You find someone unconscious in a park. You're running late for an important appointment.", 5],
-    ["You're offered 1.000€ to test a new shampoo on rabbits. It's legal, but will hurt them.", 6],
-    ["You see a child stealing food in a shop. You're the only one who saw it.", 7],
-    ["You see a friend being mean to someone anonymously online. No one knows it's them.", 8],
-    ["You're interviewing for your dream job. They ask if you've ever lied—and being honest might cost you the job.", 9],
-    ["Question 11", 10],
-    ["Question 12", 11]
+    ["Du findest eine Geldbörse mit 200€. Niemand hat es gesehen", 0],
+    ["Jemand hat dir wehgetan.", 1],
+    ["Du brauchst dringend Geld, weißt aber, dass du einen Kredit nie zurückzahlen könntest.", 2],
+    ["Dein Wecker klingelt. Du hast zur Sicherheit einen zweiten gestellt.", 3],
+    ["Ein Trolley rast auf 5 gefesselte Personen zu. Du kannst einne Hebel umlegen und damit eine andere Person töten, um die 5 zu retten.", 4],
+    ["Morgens musst du dich für ein Getränk entscheiden.", 5],
+    ["Du bist finanziell abgesichert, siehst aber, wie andere große Probleme haben.", 6],
+    ["Was machst du zuerst am Morgen?", 7],
+    ["Es könnte heute regnen.", 8],
+    ["Nicht-heterosexuell zu sein soll gesetzlich verboten werden.", 9],
+    ["Du musst dich für einen zukünftigen Job entscheiden.", 10],
+    ["Auf welcher Bettseite stehst du auf?", 11]
 ];
 const xValues = [
-    ["Keep the money.", "Try to return it."],
-    ["Lie and say you love it.", "Tell them the truth and break their heart."],
-    ["Use the answers and pass.", "Don't cheat, even if it means failing."],
-    ["Lie to save the friendship.", "Tell the truth and take the blame."],
-    ["Pull the lever.", "Do nothing."],
-    ["Call for help and stay with them.", "Leave and continue on your way."],
-    ["Take the job.", "Refuse, even if you need the money."],
-    ["Report it.", "Let it go—they probably need it."],
-    ["Call them out, risking the friendship.", "Ignore it. Not your problem."],
-    ["Lie and say you're always truthful.", "Admit you've lied before."],
-    ["Answer11/1", "Answer11/2"],
-    ["Answer12/1", "Answer12/2"]
+    ["Behalten.", "Im Fundbüro abgeben."],
+    ["Rache nehmen.", "Vergeben."],
+    ["Lügen, um den Kredit zu bekommen.", "Ehrlich bleiben und keinen Kredit erhalten."],
+    ["Jetzt aufstehen.", "Schlummern bis zum zweiten Wecker."],
+    ["Nichts tun und die 5 sterben lassen.", "Hebel ziehen und eine Person opfern."],
+    ["Kaffee.", "Tee."],
+    ["Gleichgültig bleiben.", "Geld spenden."],
+    ["Zähne putzen.", "Gesicht waschen."],
+    ["Jacke mitnehmen, was nervig ist.", "Risiko eingehen und keine mitnehmen."],
+    ["Dafür.", "Dagegen."],
+    ["Einen Job wählen, der gut bezahlt ist.", "Einen Job wählen, den du liebst."],
+    ["Links.", "Rechts."]
 ];
 
 const barColors = [
